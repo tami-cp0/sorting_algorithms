19d4119b59fdecf419b888103afe14eafff8d3da
ayo <findtamilore@gmail.com>
2023-12-16 18:07:57 +0100

    upload

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,22 @@
 #include "sort.h"
 
 /**
+* swap - Swaps the values of two integers.
+* @a: Pointer to the first integer.
+* @b: Pointer to the second integer.
+*
+* Description: This function swaps the values of the integers
+*              pointed to by 'a' and 'b'.
+*/
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
+
+/**
 * bubble_sort - Sorts an array of integers in ascending order using the
 *               Bubble Sort algorithm.
 * @array: The array of integers to be sorted.
